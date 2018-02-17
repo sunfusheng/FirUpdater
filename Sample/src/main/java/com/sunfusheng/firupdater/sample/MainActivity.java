3179ec2a7aa7c148a0ff4ee15097d45695d37e8f
sfsheng0322 <sfsheng0322@gmail.com>
2018-02-17 15:49:25 +0800

    init

---
+++ Sample/src/main/java/com/sunfusheng/firupdater/sample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sunfusheng.firupdater.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_recycler_view);
+    }
+}
