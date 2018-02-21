a9da43529f7f8139d2aa413c38f74d1d0445b07b
sfsheng0322 <sfsheng0322@gmail.com>
2018-02-21 18:44:37 +0800

    modify notification

--- FirUpdater/src/main/java/com/sunfusheng/FirNotification.java
+++ FirUpdater/src/main/java/com/sunfusheng/FirNotification.java
@@ -1,10 +1,17 @@
 package com.sunfusheng;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
+
+import static android.support.v4.app.NotificationCompat.DEFAULT_LIGHTS;
+import static android.support.v4.app.NotificationCompat.VISIBILITY_PUBLIC;
 
 /**
  * @author sunfusheng on 2018/2/17.
@@ -22,11 +29,24 @@ public class FirNotification {
         this.context = context;
         manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         builder = new NotificationCompat.Builder(context, CHANNEL_ID);
+        builder.setTicker("正在下载");
         builder.setSmallIcon(android.R.drawable.stat_sys_download);
+        Drawable icon = FirUpdaterUtils.getIcon(context);
+        if (icon != null && icon instanceof BitmapDrawable) {
+            builder.setLargeIcon(((BitmapDrawable) icon).getBitmap());
+        }
         builder.setAutoCancel(false);
         builder.setOngoing(true);
         builder.setProgress(100, 0, false);
         builder.setWhen(System.currentTimeMillis());
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 1, new Intent(), PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setFullScreenIntent(pendingIntent, false);
+        builder.setVisibility(VISIBILITY_PUBLIC);
+        builder.setContentIntent(pendingIntent);
+        builder.setDefaults(DEFAULT_LIGHTS);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            builder.setPriority(Notification.PRIORITY_HIGH);
+        }
     }
 
     public void setContentTitle(String title) {
